623c27107fd59c39358a6286333a5f1420c22fa9
nesa402610 <argyleferris@gmail.com>
2021-08-17 02:09:05 +0300

    Типо фикс железок

--- resources/views/hardware.blade.php
+++ resources/views/hardware.blade.php
@@ -26,24 +26,36 @@
                     @foreach ($hardwares as $hardware)
                         @if ($hardware->product->visibility == 1)
                             <div id="card" class="buying_card">
-                                <div astyle="background-image: url('/storage/products_img/{{ $hardware->product->image }}') ">
+                                <div
+                                    astyle="background-image: url('/storage/products_img/{{ $hardware->product->image }}') ">
                                     <div class="hardware_title">
                                         <a
                                             href="{{ route('hardwareView', $hardware->id . '-' . $hardware->product->slug) }}">{{ $hardware->product->name }}</a>
                                     </div>
                                     <div class="hardware_content">
-                                        <img src="/storage/products_img/{{ $hardware->product->image }}" alt="">
+                                        <div class="img">
+                                            {{-- <div class="product-status">
+                                                @if ($hardware->product->quantity >= 1)
+                                                    <div>В наличии</div>
+                                                @endif
+                                            </div> --}}
+                                            <img src="/storage/products_img/{{ $hardware->product->image }}" alt="">
+                                        </div>
                                     </div>
                                     <div class="hardware_footer">
                                         <div class="price">
                                             {{ $hardware->product->price }} рублей
                                         </div>
-                                        <form class="form_buy"
-                                            action="{{ route('cart.add', ['id' => $hardware->product->id]) }}"
-                                            method="post">
-                                            @CSRF
-                                            <button type="submit" class="buy-button" name="button">Купить</button>
-                                        </form>
+                                        @if ($hardware->product->quantity >= 1)
+                                            <form class="form_buy"
+                                                action="{{ route('cart.add', ['id' => $hardware->product->id]) }}"
+                                                method="post">
+                                                @CSRF
+                                                <button type="submit" class="buy-button" name="button">Купить</button>
+                                            </form>
+                                        @else
+                                            <button type="submit" class="buy-button" name="button">Нет в наличии</button>
+                                        @endif
                                     </div>
                                 </div>
                             </div>
