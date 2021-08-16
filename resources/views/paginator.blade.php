073f92e1f60d8fe79139e5159f1a6f97f17a74a2
nesa402610 <argyleferris@gmail.com>
2021-08-17 00:14:07 +0300

    Добавил пагинацию и исправил ее

--- resources/views/paginator.blade.php
+++ resources/views/paginator.blade.php
@@ -1,22 +1,62 @@
-<div id="paginator">
-    @if (!$paginate->onFirstPage())
-    <div>
-        <a href="{{ $paginate->url(1) }}">Первая страница</a>
+@if ($paginate->hasPages())
+    <div id="paginator">
+        @if (!$paginate->onFirstPage())
+            <div>
+                <a href="{{ $paginate->url(1) }}">Первая страница</a>
+            </div>
+            <div>
+                <a href="{{ $paginate->previousPageUrl() }}">
+                    << </a>
+            </div>
+        @else
+            <div>
+                <a class="disabled">Первая страница</a>
+            </div>
+            <div>
+                <a class="disabled">
+                    << </a>
+            </div>
+        @endif
+        @if ($paginate->currentPage() >= 3)
+            <div>
+                <a href="{{ $paginate->url($paginate->currentPage() - 2) }}">{{ $paginate->currentPage() - 2 }}</a>
+            </div>
+        @endif
+        @if ($paginate->currentPage() >= 2)
+            <div>
+                <a href="{{ $paginate->url($paginate->currentPage() - 1) }}">{{ $paginate->currentPage() - 1 }}</a>
+            </div>
+        @endif
+        <div>
+            <a class="baka">{{ $paginate->currentPage() }}</a>
+        </div>
+        @if ($paginate->currentPage() !== $paginate->lastPage() && $paginate->currentPage() <= $paginate->lastPage() -2)
+            <div>
+                <a href="{{ $paginate->url($paginate->currentPage() + 1) }}">{{ $paginate->currentPage() + 1 }}</a>
+            </div>
+            <div>
+                <a href="{{ $paginate->url($paginate->currentPage() + 2) }}">{{ $paginate->currentPage() + 2 }}</a>
+            </div>
+        @elseif ($paginate->currentPage() == $paginate->lastPage() -1)
+            <div>
+                <a
+                    href="{{ $paginate->url($paginate->currentPage() + 1) }}">{{ $paginate->currentPage() + 1 }}</a>
+            </div>
+        @endif
+        @if ($paginate->currentPage() !== $paginate->lastPage())
+            <div>
+                <a href="{{ $paginate->nextPageUrl() }}">>></a>
+            </div>
+            <div>
+                <a href="{{ $paginate->url($paginate->lastPage()) }}">Последняя страница</a>
+            </div>
+        @else
+            <div>
+                <a class="disabled">>></a>
+            </div>
+            <div>
+                <a class="disabled">Последняя страница</a>
+            </div>
+        @endif
     </div>
-    <div>
-        <a href="{{ $paginate->previousPageUrl() }}">Предыдущая</a>
-    </div>
-    @endif
-    <div>
-        <a class="baka">Текущая страница {{ $paginate->currentPage() }}</a>
-    </div>
-    <div>
-        <a href="{{ $paginate->nextPageUrl() }}">Следующая страница</a>
-    </div>
-    <div>
-        <a href="">Послденяя страница {{ $paginate->lastPage() }}</a>
-    </div>
-        @foreach ($paginate->getUrlRange(1, 3) as $a)
-            {{ $a }}
-        @endforeach
-</div>
+@endif
